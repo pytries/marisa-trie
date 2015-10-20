05d349c64887dd41e9f6ad6d9d005688926a32c2
Sergei Lebedev <superbobry@gmail.com>
2015-10-20 23:06:03 +0300

    Added Python3.5 to ``setup.py`` and ``.travis.yml``

--- setup.py
+++ setup.py
@@ -46,6 +46,7 @@ setup(
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Scientific/Engineering :: Information Analysis',

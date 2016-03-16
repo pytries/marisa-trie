db866fe013eb2ee19e431323b8825a297f2b8e34
Sergei Lebedev <superbobry@gmail.com>
2016-03-16 15:33:18 +0300

    ``setup.py`` revamp

--- setup.py
+++ setup.py
@@ -1,53 +1,71 @@
 #! /usr/bin/env python
+"""Static memory-efficient and fast Trie-like structures for Python."""
+
 import glob
 import itertools
-from setuptools import setup
-from distutils.extension import Extension
 
+from setuptools import setup, Extension
+
+MARISA_DIR = "lib"
 MARISA_FILES = [
-    'lib/marisa/*.cc',
-    'lib/marisa/grimoire.cc',
-    'lib/marisa/grimoire/io/*.cc',
-    'lib/marisa/grimoire/trie/*.cc',
-    'lib/marisa/grimoire/vector/*.cc',
+    "lib/marisa/*.cc",
+    "lib/marisa/grimoire.cc",
+    "lib/marisa/grimoire/io/*.cc",
+    "lib/marisa/grimoire/trie/*.cc",
+    "lib/marisa/grimoire/vector/*.cc",
 ]
 
 MARISA_FILES = list(itertools.chain(*(glob.glob(path) for path in MARISA_FILES)))
 
-setup(
-    name="marisa-trie",
-    version="0.7.2",
-    description="Static memory-efficient & fast Trie-like structures for Python (based on marisa-trie C++ library)",
-    long_description = open('README.rst').read() + "\n\n" + open('CHANGES.rst').read(),
-    author='Mikhail Korobov',
-    author_email='kmike84@gmail.com',
-    url='https://github.com/kmike/marisa-trie/',
+DESCRIPTION = __doc__
+LONG_DESCRIPTION = open("README.rst").read() + open("CHANGES.rst").read()
+LICENSE = "MIT"
 
-    ext_modules = [
-        Extension(
-            "marisa_trie",
-            sources = glob.glob('src/*.cpp') + MARISA_FILES,
-            include_dirs=['lib'],
-            language = "c++",
-        )
-    ],
+CLASSIFIERS = [
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Cython",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.6",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Topic :: Scientific/Engineering :: Information Analysis",
+    "Topic :: Text Processing :: Linguistic",
+]
 
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Cython',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Scientific/Engineering :: Information Analysis',
-        'Topic :: Text Processing :: Linguistic',
-    ],
-)
+setup(name="marisa-trie",
+      version="0.7.2",
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      author="Mikhail Korobov",
+      author_email="kmike84@gmail.com",
+      license=LICENSE,
+      url="https://github.com/kmike/marisa-trie",
+      classifiers=CLASSIFIERS,
+      libraries=[("libmarisa-trie", {
+          "sources": MARISA_FILES,
+          "include_dirs": [MARISA_DIR]
+      })],
+      ext_modules=[
+          Extension("marisa_trie", [
+              "src/agent.cpp",
+              "src/base.cpp",
+              "src/iostream.cpp",
+              "src/key.cpp",
+              "src/keyset.cpp",
+              "src/marisa_trie.cpp",
+              "src/query.cpp",
+              "src/std_iostream.cpp",
+              "src/trie.cpp"
+          ], include_dirs=[MARISA_DIR])
+      ],
+
+      setup_requires=["pytest-runner"],
+      tests_require=["pytest"])
